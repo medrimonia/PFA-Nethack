2e1fbd7c625e18ff8df452b4cc8a42c6acb301fe
Ludovic Hofer <medrimonia@gmail.com>
2012-12-17 16:02:10 +0100

    LaunchBot added

---
+++ java_starter_package/src/LaunchBot.java
@@ -0,0 +1,14 @@
+import bot.Bot;
+
+public class LaunchBot{
+
+	public static void main(String[] args){
+		try{
+			Bot bot = new Bot("localhost");
+			bot.start();
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
