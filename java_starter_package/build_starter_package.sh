d39b3671707580a2fb0d6d64057d469dc5d761b3
Ludovic Hofer <medrimonia@gmail.com>
2012-12-20 21:23:14 +0100

    script to build the starter package improved, can now be launched from any directory

--- java_starter_package/build_starter_package.sh
+++ java_starter_package/build_starter_package.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+BASEDIR=$(pwd)
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+echo "Switching to script directory"
+cd $DIR
+echo "Compiling..."
 find -name "*.java" | xargs javac -d bin
 cd bin
+echo "Creating jar..."
 find -name "*.class" | xargs jar -cfe ../Bot.jar LaunchBot
 cd ..
+cd $BASEDIR
+echo "Back to original directory"
