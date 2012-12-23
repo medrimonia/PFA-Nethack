cdb31f229150d4b85a0eb1bf87acb2cd62b7f1fa
David <dbitonneau@enseirb-matmeca.fr>
2012-12-23 15:13:48 +0100

    Répercution des changements de master

--- nh-setup.sh
+++ nh-setup.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-nh="$HOME/games/nethack"
 nhdir="nethack-3.4.3/"
 nharchive="nethack-343-src.tgz"
 dlurl="http://downloads.sourceforge.net/project/nethack/nethack/3.4.3/nethack-343-src.tgz"
-patchdir="../patch/"
+patchdir="patches"
 
 apply_patch () {
     for i in `ls $patchdir`; do
         read -p "Apply $i? [Y/n]" yn
 
         case $yn in
-            Y|y|"" ) patch -p2 < $patchdir$i;;
+            Y|y|"" ) patch -p2 < $patchdir/$i;;
             * ) ;;
         esac
     done
@@ -19,7 +18,7 @@ apply_patch () {
 
 dl_nethack () {
     if [ ! -e $nharchive ]; then
-        read -p "$nharchive not found, automaticaly download it now? [Y/n]" yn
+        read -p "$nharchive not found, automatically download it now? [Y/n]" yn
 
         case $yn in
             Y|y|"" ) wget $dlurl;;
@@ -47,15 +46,15 @@ else
 fi
 
 
-if [ -e $nh ]; then
-    read -p "previous nethack installation found at $nh, overwrite? [y/n]" yn
-
-    case $yn in
-        Y|y ) ;;
-        N|n ) exit;;
-        * ) echo "please answer 'y' or 'n'"; exit;;
-    esac
-fi
+#if [ -e $nh ]; then
+#    read -p "previous nethack installation found at $nh, overwrite? [y/n]" yn
+#
+#    case $yn in
+#        Y|y ) ;;
+#        N|n ) exit;;
+#        * ) echo "please answer 'y' or 'n'"; exit;;
+#    esac
+#fi
 
 
 if [ $reuse = 0 ]; then
@@ -74,5 +73,5 @@ fi
 cd $nhdir && make && make install
 
 if [ $? = 0 ]; then
-    echo "Nethack run script installed at $nh"
+    echo "Nethack run script installed in $nhdir"
 fi
