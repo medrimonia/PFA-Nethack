621f3ba3634c0626091bd2864e1ed9bbb869d3f8
Ludovic Hofer <medrimonia@gmail.com>
2013-02-05 01:03:26 +0100

    Trying to use slices in diffusion bot

--- diffusion_bot/src/util/Scoring.java
+++ diffusion_bot/src/util/Scoring.java
@@ -2,21 +2,25 @@ package util;
 
 public class Scoring {
 	
-	public final static double DISTANCE_FACTOR = 0.8;
+	public final static double DISTANCE_FACTOR = 0.9;
 	
 	private final static double SEARCH_VALUE = 0.98;
 	private final static double VISIT_VALUE = 0.004;
 	private final static double OPEN_VALUE = 0.85;
+	/** In order to avoid permanent way and back, steps are crossed
+	 * every SLICE_SIZE steps
+	 */
+	private final static double SLICE_SIZE = 3;
 	
 	public static double searchScore(int nbSearch){
-		return Math.pow(SEARCH_VALUE, nbSearch);
+		return Math.pow(SEARCH_VALUE, SLICE_SIZE * (nbSearch % SLICE_SIZE));
 	}
 	
 	public static double visitScore(int nbVisits){
-		return Math.pow(VISIT_VALUE, nbVisits);
+		return Math.pow(VISIT_VALUE, SLICE_SIZE * (nbVisits % SLICE_SIZE));
 	}
 	
 	public static double openScore(int nbTries){
-		return Math.pow(OPEN_VALUE, nbTries);
+		return Math.pow(OPEN_VALUE, SLICE_SIZE * (nbTries % SLICE_SIZE));
 	}
 }
