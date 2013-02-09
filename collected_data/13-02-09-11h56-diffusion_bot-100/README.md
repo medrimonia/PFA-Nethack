e4ee048dc7662971ed30e3a63a8de755af66623d
Ludovic Hofer <medrimonia@gmail.com>
2013-02-09 21:38:55 +0100

    adding a new type of game_runner, not fully tested now and adding some data collected

---
+++ collected_data/13-02-09-11h56-diffusion_bot-100/README.md
@@ -0,0 +1,24 @@
+## Source Data
+* Number of games : 100
+* Bot : diffusion_bot
+* Max moves : 
+* Current Commit : f8876d1de7dfe962dbd3a9bcc9a0755a0c776c3b
+* Processing time: 7hours0m40s
+
+##Bot details
+* Constants :
+    * MOVE_COST = 3
+    * MINIMAL_CONSERVATION = 1
+    * PERCENT_CONSERVATION = 0.4
+    * VISIT_SCORE = 1
+    * UNKNOWN_SCORE = 2
+    * PASSAGE_SCORE = 20
+    * OPEN_SCORE = 50
+    * OPEN_FADING = 0.8
+    * FORCE_SCORE = 50
+    * FORCE_FADING = 0.8
+    * FOUND_SCORE = 2500
+    * DEAD_END_SEARCH_SCORE = 1
+    * CLASSIC_SEARCH_SCORE = 0.01
+    * SEARCH_K = 1
+    * OPEN_K = 1
---
+++ game_runner.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+STARTM=$(date -u "+%s")
+
+#default options
+NH_MAX_MOVES=200
+NH_BOT_NAME="java_sp"
+NH_MM_LOGGING=0
+NH_DATABASE_PATH="/tmp/test.db"
+NB_GAMES=10
+BOT_PATH="java_starter_package/Bot.jar"
+BOT_CMD="java -jar"
+
+
+#b specifies the bot
+#l enable middleman logging
+#m specifies the number of moves per game
+#g specifies the number of games
+#p specifies the bot path
+#c specifies the bot launching cmd
+#d specifies the database path
+while getopts "g:m:b:p:c:d:l" opt; do
+		case $opt in
+				g)
+						NB_GAMES=$OPTARG;
+						;;
+				m)
+						NH_MAX_MOVES=$OPTARG;
+						;;
+				b)
+						NH_BOT_NAME=$OPTARG;
+						;;
+				p)
+						BOT_PATH=$OPTARG;
+						;;
+				c)
+						BOT_CMD=$OPTARG;
+						;;
+				d)
+						NH_DATABASE_PATH=$OPTARG;
+						;;
+				l)
+						NH_MM_LOGGING=1;
+						;;
+		esac
+done
+
+NH_MM_SOCKPATH="/tmp/mmsock"$$
+
+export NH_MM_SOCKPATH
+export NH_MAX_MOVES
+export NH_MM_LOGGING
+export NH_BOT_NAME
+export NH_DATABASE_PATH
+
+#Some tests should be runned first
+
+
+TEST_FOLDER="test"$$	
+BOT_FILE=$(basename $BOT_PATH)
+
+mkdir $TEST_FOLDER
+#content to move should be improved
+cp -r nethack-3.4.3 $TEST_FOLDER
+cp $BOT_PATH $TEST_FOLDER
+
+for ((i = 1; i <= NB_GAMES; i++))
+do
+		#Avoiding mm.log to grow too much in size
+		rm -f $TEST_FOLDER/nethack-3.4.3/nethackdir/mm.log
+		#Running nethack
+		$TEST_FOLDER/nethack-3.4.3/nethack & #>$TEST_FOLDER/nh_log &
+		#Running bot
+		$BOT_CMD $TEST_FOLDER/$BOT_FILE $NH_MM_SOCKPATH #>$TEST_FOLDER/bot_log
+		printf "\033[80DRunning tests : %d/%d" $i $NB_GAMES
+done
+echo
