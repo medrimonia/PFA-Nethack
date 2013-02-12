b96ae986bd0f6f10b2b4a56ab83708caff91725a
uOptim <uOptim@fakeemail.com>
2013-02-13 00:56:16 +0100

    viewer can jump directly to specified turn

--- viewer/viewer.pl
+++ viewer/viewer.pl
@@ -5,30 +5,69 @@ use Time::HiRes qw/sleep/;
 use Term::ANSIScreen qw/:cursor :screen/;
 
 
+
 my $filename = $ARGV[0] // "replay";
 open(my $fh, '<', $filename) or die "Can't open file $filename: $!";
 $/ = undef; #slurp!
 my $replay = <$fh>;
-
 close $fh;
 
-cls;
-$| = 1;
 
+cls();
+$| = 1;
+$/ = "\n";
+
+my $i = 1;
 my @coms = split("ES", $replay);
 
-for (@coms) {
-	my @glyphs = split('g');
+my @glyphs = split('g', $coms[0]);
+print_glyphs(@glyphs);
 
-	for (@glyphs) {
+while (1) {
+	# Prompt
+	locate 1,1;
+	clline();
+	print "> ";
+	my $nbr = <STDIN>;
+
+	# goto turn number $nbr
+	if ($nbr =~ /^\d+$/ && $nbr <= $#coms) {
+		my $start;
+
+		if ($i < $nbr) {
+			$start = $i + 1;
+		} else {
+			cls();
+			$start = 0;
+		}
+
+		for ($start .. $nbr) {
+			@glyphs = split('g', $coms[$_]);
+			print_glyphs(@glyphs);
+		}
+
+		$i = $nbr;
+	}
+	
+	else {
+		$i++;
+		@glyphs = split('g', $coms[$i]);
+		print_glyphs(@glyphs);
+	}
+
+	#sleep(0.01);
+}
+
+
+sub print_glyphs {
+	local $| = 0;
+
+	for (@_) {
 		my ($y, $x, $g) = unpack("WWa");
 
 		if (defined $g) {
-			locate $x, $y;
+			locate $x+1, $y+1;
 			print $g;
 		}
 	}
-
-	sleep(0.05);
 }
-
