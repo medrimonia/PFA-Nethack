aef892983445b2da747625138a72a3f184791fe1
Ludovic Hofer <medrimonia@gmail.com>
2013-03-04 23:01:34 +0100

    added average level graph handling to move_graph.sh (not tested)

--- scripts/move_graph.sh
+++ scripts/move_graph.sh
@@ -24,27 +24,39 @@ MOVE_COLUMN=2
 FIELDS[0]="percent_explored"
 DATA_COLUMN[0]=3
 FIELD_NAME[0]="squares explored"
+YRANGE[0]=110
 FIELDS[1]="percent_sdoors_found"
 DATA_COLUMN[1]=4
 FIELD_NAME[1]="secret doors found"
+YRANGE[1]=110
 FIELDS[2]="percent_scorrs_found"
 DATA_COLUMN[2]=5
 FIELD_NAME[2]="secret corridors found"
+YRANGE[2]=110
+FIELDS[3]="level_reached"
+DATA_COLUMN[3]=6
+FIELD_NAME[3]="average level reached"
+YRANGE[3]=$(sqlite3 $DATABASE 'select max(level_reached) from games')
 
 #Generating data files
 for ((i = 0; i < ${#BOTS[@]}; i++))
 do
-		BOTS[i]=$(echo ${BOT_FILE[i]} | sed 's/-/ /')
-		echo "Bot file : ${BOT_FILE[i]}"
+    BOTS[i]=$(echo ${BOT_FILE[i]} | sed 's/-/ /')
+    echo "Bot file : ${BOT_FILE[i]}"
     FILES[i]="${DB_PATH}/${BOT_FILE[i]}.csv"
     REQUEST="'select count(*) as nb_games,
                      max_moves,
-                     100*sum(nb_squares_explored)/ sum(nb_squares_reachable) as percent_explored,
-                     100*sum(nb_sdoor_discovery)/sum(nb_sdoors) as percent_sdoors_found,
-                     100*sum(nb_scorr_discovery)/sum (nb_scorrs) as percent_scorrs_found
+                     100*sum(nb_squares_explored)/ sum(nb_squares_reachable)
+                         as percent_explored,
+                     100*sum(nb_sdoor_discovery)/sum(nb_sdoors)
+                         as percent_sdoors_found,
+                     100*sum(nb_scorr_discovery)/sum (nb_scorrs)
+                         as percent_scorrs_found,
+                     avg(level_reached)
+                         as level_reached
               from game_results
               where bot_name == \"${BOTS[i]}\" group by max_moves;'"
-		echo "redirection to ${FILES[i]}"
+    echo "redirection to ${FILES[i]}"
     # This particular syntax is due to a problem when processing argument with '...'
     echo $REQUEST | xargs sqlite3 -header -csv $DATABASE >${FILES[i]}
 done
