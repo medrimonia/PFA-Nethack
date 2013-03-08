89a7f94a39cf6501417c64aeec97e1da130a78e6
David <dbitonneau@enseirb-matmeca.fr>
2013-03-08 11:35:05 +0100

    Help added in game_runner + special_bot updated

--- game_runner.sh
+++ game_runner.sh
@@ -11,6 +11,13 @@ NB_GAMES=10
 BOT_PATH="java_starter_package/Bot.jar"
 BOT_CMD="java -jar"
 
+usage() {
+	echo "Usage: $0 [options]"
+	echo "Options:"
+	echo -e "\t-h        display this help"
+	echo "Exemple: $0 -b \"python sp\" -p \"pythonsp/bot.py\" -c \"python\""
+}
+
 
 #b specifies the bot name (Unusual chars should be avoided, spaces are accepted)
 #l enable middleman logging
@@ -19,8 +26,12 @@ BOT_CMD="java -jar"
 #p specifies the bot path
 #c specifies the bot launching cmd
 #d specifies the database path
-while getopts "g:m:b:p:c:d:l" opt; do
+while getopts "g:m:b:p:c:d:lh" opt; do
 		case $opt in
+				h)
+						usage;
+						exit;
+						;;
 				g)
 						NB_GAMES=$OPTARG;
 						;;
@@ -73,8 +84,6 @@ cp pythonsp/nhmap.py $TEST_FOLDER
 
 for ((i = 1; i <= NB_GAMES; i++))
 do
-		#dirtyhack
-		cp pythonsp/nhmap.py $TEST_FOLDER
 		#Avoiding mm.log to grow too much in size
 		rm -f $TEST_FOLDER/nethack-3.4.3/nethackdir/mm.log
 		#Running nethack
